f98636a16d8c6c7e5c6a256b992f3408d0825f3a
wiem77 <wiembatti77@gmail.com>
2023-04-18 04:04:02 +0100

    OtpScreen Commit

--- frontend/navigation/Navigation.js
+++ frontend/navigation/Navigation.js
@@ -5,11 +5,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
 import WelcomeScreen from '../screens/WelcomeScreen/WelcomeScreen';
 import SignUpScreen from '../screens/SignUpScreen/SignUpScreen';
+import OtpScreen from '../screens/otpScreen/OtpScreen';
 
 const ConsumerStack = createNativeStackNavigator();
 const ConsumerNavigation = () => {
   return (
     <ConsumerStack.Navigator screenOptions={{ headerShown: false }}>
+      <ConsumerStack.Screen name="OtpScreen" component={OtpScreen} />
       <ConsumerStack.Screen name="WelcomeScreen" component={WelcomeScreen} />
       <ConsumerStack.Screen name="LoginIn" component={LoginScreen} />
       <ConsumerStack.Screen name="SignUp" component={SignUpScreen} />

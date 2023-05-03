55f2143693cdb0176236883cd9a715d64b8ded78
wiem77 <wiembatti77@gmail.com>
2023-05-03 15:09:36 +0100

    UpdateVoucher Commit

--- server/controllers/voucherController/voucherController.js
+++ server/controllers/voucherController/voucherController.js
@@ -50,3 +50,29 @@ module.exports.createVoucher = async (req, res) => {
       .json({ message: 'An error occurred while creating the voucher' });
   }
 };
+
+module.exports.updateVoucher = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name_V, description, validity_date, available_vouchers, discount, is_available } = req.body;
+  
+      let voucher = await Voucher.findById(id);
+      if (!voucher) {
+        return res.status(404).json({ message: 'Voucher not found' });
+      }
+  
+      voucher.name_V = name_V ?? voucher.name_V;
+      voucher.description = description ?? voucher.description;
+      voucher.validity_date = validity_date ?? voucher.validity_date;
+      voucher.available_vouchers = available_vouchers ?? voucher.available_vouchers;
+      voucher.discount = discount ?? voucher.discount;
+      voucher.is_available = is_available !== undefined ? is_available : voucher.is_available;
+  
+      const updatedVoucher = await voucher.save();
+      res.status(200).json(updatedVoucher);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'An error occurred while updating the voucher' });
+    }
+  };
+  
--- server/routes/voucherRoutes/voucherRoutes.js
+++ server/routes/voucherRoutes/voucherRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const {
   createVoucher,
+  updateVoucher,
 } = require('../../controllers/voucherController/voucherController');
 const router = express.Router();
 
 //root:http://localhost:4000/api/vouchers/addVoucher
 router.post('/addVoucher', createVoucher);
+
+//root:http://localhost:4000/api/vouchers/updateVoucher/:id
+router.put('/updateVoucher/:id', updateVoucher);
 module.exports = router;

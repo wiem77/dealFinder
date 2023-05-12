680a322aa78e0eec439c203c61137f21be8d6478
wiem77 <wiembatti77@gmail.com>
2023-05-12 14:12:51 +0100

    added new rout navigation commit

--- FrontEndStore/App.js
+++ FrontEndStore/App.js
@@ -3,14 +3,38 @@ import { StyleSheet, Text, View } from 'react-native';
 import { customFonts } from './config/config';
 import Navigation from './navigation/Navigation';
 import { useFonts } from 'expo-font';
+import { AuthProvider, AuthContext } from './context/AuthProvider';
+import { NavigationContainer } from '@react-navigation/native';
+import { useContext, useEffect, useState } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ScanQrScreen from './Screens/ScanQrScreen/ScanQrScreen';
+import LoginScreen from './Screens/LoginScreen/LoginScreen';
+import VerificationScreen from './Screens/VerificationScreen/Verification';
+import SuccessVerification from './Screens/sucessScreen/SucessVerification';
+const StoreStack = createNativeStackNavigator();
+
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [fontsLoaded] = useFonts(customFonts);
+
   if (!fontsLoaded) {
     return <Text>Loading...</Text>;
   }
+
+  // const { user } = useContext(AuthContext);
+
   return (
-    <>
-      <Navigation />
-    </>
+    <AuthProvider>
+      <NavigationContainer>
+        <StoreStack.Navigator>
+          <StoreStack.Screen name="ScanQr" component={ScanQrScreen} />
+          <StoreStack.Screen name="Success" component={SuccessVerification} />
+
+          <StoreStack.Screen name="VerifF" component={VerificationScreen} />
+
+          <StoreStack.Screen name="Login" component={LoginScreen} />
+        </StoreStack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
--- FrontEndStore/navigation/Navigation.js
+++ FrontEndStore/navigation/Navigation.js
@@ -1,22 +1,26 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../Screens/LoginScreen/LoginScreen';
+import ScanQrScreen from '../Screens/ScanQrScreen/ScanQrScreen';
+import { AuthContext } from '../context/AuthProvider';
 
 const StoreStack = createNativeStackNavigator();
 const StoreNavigation = () => {
   return (
     <StoreStack.Navigator screenOptions={{ headerShown: false }}>
       <StoreStack.Screen name="LoginStore" component={LoginScreen} />
+      <StoreStack.Screen name="ScanQr" component={ScanQrScreen} />
     </StoreStack.Navigator>
   );
 };
-const Navigation = () => {
-  return (
+const AuthNavigation = () => {
+  const {isLoading,}=useContext(AuthContext)
+    return (
     <NavigationContainer>
       <StoreNavigation />
     </NavigationContainer>
   );
 };
-export default Navigation;
+export default AuthNavigation;

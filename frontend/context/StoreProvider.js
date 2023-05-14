94330031d51fa320e43bf20aca5aa7ef03a44efe
wiem77 <wiembatti77@gmail.com>
2023-05-14 13:51:23 +0100

    set satoresContext commit

--- frontend/context/StoreProvider.js
+++ frontend/context/StoreProvider.js
@@ -1,83 +1,103 @@
-import React, {
-  useState,
-  createContext,
-  useEffect,
-  useCallback,
-  useMemo,
-} from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import axios from 'axios';
 import { baseUrl } from '../config/config';
-import { AsyncStorage } from 'react-native';
-
-export const StoresContext = createContext({
-  stores: null,
-});
+import AsyncStorage from '@react-native-async-storage/async-storage';
+export const StoreContext = createContext();
 
 export const StoresProvider = ({ children }) => {
-  const [stores, setStores] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchStoresData = async () => {
-    try {
-      const response = await axios.get(`${baseUrl}/store/getAllStore`);
-      const storesObject = response.data.reduce((acc, cur) => {
-        acc[cur._id] = {
-          ...cur,
-          sub_categories: cur.sub_categories.reduce((subAcc, subCur) => {
-            subAcc[subCur._id] = subCur;
-            return subAcc;
-          }, {}),
-          locations: cur.locations.reduce((locAcc, locCur) => {
-            locAcc[locCur._id] = locCur;
-            return locAcc;
-          }, {}),
-          vouchers: cur.vouchers.reduce((vouAcc, vouCur) => {
-            vouAcc[vouCur._id] = vouCur;
-            return vouAcc;
-          }, {}),
-        };
-        return acc;
-      }, {});
-
-      console.log(storesObject);
-      return storesObject;
-    } catch (error) {
-      console.log(error);
-      throw new Error('An error occurred while fetching stores');
-    }
-  };
+  const [stores, setStores] = useState([]);
 
   useEffect(() => {
-    let isMounted = true;
-
-    if (!stores) {
-      fetchStoresData()
-        .then((storesObject) => {
-          if (isMounted) {
-            setStores(storesObject);
-            setIsLoading(false);
-            console.log('storesssss', stores);
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-          setIsLoading(false);
-        });
-    } else {
-      setIsLoading(false);
-    }
-
-    return () => {
-      isMounted = false;
+    const fetchStores = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/store/getAllStore`);
+        const data = response.data;
+        setStores(data);
+        await AsyncStorage.setItem('stores', JSON.stringify(response.data));
+        console.log(
+          'Data fetcheddddddddddddddd:',
+          JSON.stringify(response.data)
+        );
+      } catch (error) {
+        console.error(error);
+      }
     };
+
+  
+    AsyncStorage.getItem('stores')
+      .then((data) => {
+        if (data) {
+          setStores(JSON.parse(data));
+          console.log('testttt', data);
+        } else {
+          fetchStores();
+        }
+      })
+      .catch((error) => console.error(error));
   }, []);
 
   return (
-    <StoresContext.Provider value={{ stores, isLoading }}>
-      {children}
-    </StoresContext.Provider>
+    <StoreContext.Provider value={{ stores }}>{children}</StoreContext.Provider>
   );
 };
+
+// export const StoresContext = createContext({
+//   stores: null,
+//   getStores: null,
+//   getAllStores: () => {},
+// });
+
+// export const StoresProvider = ({ children }) => {
+//   const [stores, setStores] = useState(null);
+//   const [getStores, setGetStores] = useState(() => getAllStores);
+//   async function getAllStores() {
+//     if (stores) {
+//       // Les données de magasin ont déjà été récupérées, inutile d'envoyer une autre requête.
+//       return;
+//     }
+
+//     const stores = await getStores();
+//     setStores(stores);
+//     console.log('JSON.stringify(stores)', JSON.stringify(stores));
+//     AsyncStorage.setItem('stores', JSON.stringify(stores));
+//     setGetStores(true);
+//   }
+
+//   const value = {
+//     getStores,
+//     getAllStores,
+//     stores,
+//   };
+
+//   return (
+//     <StoresContext.Provider value={value}>{children}</StoresContext.Provider>
+//   );
+// };
+
+// useEffect(() => {
+//   let isMounted = true;
+
+//   if (!stores) {
+//     fetchStoresData()
+//       .then((storesObject) => {
+//         if (isMounted) {
+//           setStores(storesObject);
+//           setIsLoading(false);
+//           console.log('storesssss', stores);
+//         }
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         setIsLoading(false);
+//       });
+//   } else {
+//     setIsLoading(false);
+//   }
+
+//   return () => {
+//     isMounted = false;
+//   };
+// }, []);
 
 // export const StoresProvider = ({ children }) => {
 //   const [stores, setStores] = useState(null);

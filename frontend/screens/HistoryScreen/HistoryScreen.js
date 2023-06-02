67d7db270f6909ff1682115474eeddba5a2eb8a9
wiem77 <wiembatti77@gmail.com>
2023-06-02 20:14:47 +0100

    HistoryScreen finalecommit

--- frontend/screens/HistoryScreen/HistoryScreen.js
+++ frontend/screens/HistoryScreen/HistoryScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useStateuseEffect } from 'react';
 import {
   View,
   Text,
@@ -9,7 +9,14 @@ import {
 import { Ionicons, Feather, FontAwesome5 } from '@expo/vector-icons';
 import { Colors } from '../../constants/Colors';
 import { AuthContext } from '../../context/AuthProvider';
-import { ListItem } from 'react-native-elements';
+import { ListItem, colors } from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { baseUrl } from '../../config/config';
+import { useFocusEffect } from '@react-navigation/native';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Loading2 from '../../components/loading2/Loading2';
 const reservedVouchers = [
   {
     id: 1,
@@ -36,11 +43,43 @@ const HistoryScreen = () => {
   const authCtx = useContext(AuthContext);
   const token = authCtx.token;
   const user = authCtx.user;
-  console.log('userrazerazeze', user);
-  // const reservedVouchers = user.reservedVouchers;
-  const [voucherStates, setVoucherStates] = useState({});
 
-  const handleLike = (id) => {
+  const [voucherStates, setVoucherStates] = useState({});
+  const [historyData, setHistoryData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchData = async () => {
+        try {
+          const storedData = await AsyncStorage.getItem('historyData');
+          if (storedData) {
+            const response = await axios.get(
+              `${baseUrl}/reservation/userReservation/usedTrue/${user._id}`,
+              {
+                headers: {
+                  'x-access-token': token,
+                },
+              }
+            );
+            const data = response.data;
+            await AsyncStorage.setItem('historyData', JSON.stringify(data));
+            setHistoryData(data);
+          } else {
+            setHistoryData([]);
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+      fetchData();
+    }, [])
+  );
+
+  const handleLike = async (id) => {
     setVoucherStates((prevState) => ({
       ...prevState,
       [id]: {
@@ -48,9 +87,24 @@ const HistoryScreen = () => {
         disliked: false,
       },
     }));
+
+    try {
+      await axios.post(
+        `${baseUrl}/users/newRating/${user._id}/${id}`,
+        { ratingValue: 1 },
+        {
+          headers: {
+            'x-access-token': token,
+          },
+        }
+      );
+      console.log('test1');
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const handleDislike = (id) => {
+  const handleDislike = async (id) => {
     setVoucherStates((prevState) => ({
       ...prevState,
       [id]: {
@@ -58,67 +112,126 @@ const HistoryScreen = () => {
         disliked: true,
       },
     }));
+
+    try {
+      await axios.post(
+        `${baseUrl}/users/newRating/${user._id}/${id}`,
+        { ratingValue: -1 },
+        {
+          headers: {
+            'x-access-token': token,
+          },
+        }
+      );
+      console.log('test');
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  console.log('reservedVouchers', reservedVouchers);
-  const renderVoucherItem = ({ item }) => (
-    <ListItem bottomDivider containerStyle={styles.voucherItem}>
-      <ListItem.Content>
-        <View style={styles.headerContainer}>
-          <Feather
-            name="shopping-bag"
-            size={24}
-            color="black"
-            style={styles.icon}
-          />
-          <ListItem.Title style={styles.boutique}>
-            {item.voucher.store.store_name}
-          </ListItem.Title>
-        </View>
-        <ListItem.Title style={styles.gain}>
-          Coupon: {item.voucher.name_V}
-        </ListItem.Title>
-        <ListItem.Title style={styles.gain}>
-          Réduction: {item.voucher.discount}
-        </ListItem.Title>
-        <View style={styles.buttonsContainer}>
-          <Text style={[styles.noteText, { marginRight: 8 }]}>
-            Noter la boutique:
-          </Text>
-          <TouchableOpacity onPress={() => handleLike(item.id)}>
-            {voucherStates[item.id]?.liked ? (
-              <Ionicons
-                name="thumbs-up"
-                size={24}
-                color="#4CAF50" // Couleur verte pour le like
-                style={styles.button}
-              />
-            ) : (
-              <Ionicons
-                name="thumbs-up-outline"
-                size={24}
-                color="black" // Couleur par défaut pour le like
-                style={styles.button}
-              />
-            )}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => handleDislike(item.id)}>
-            <Ionicons
-              name="thumbs-down"
+  const renderVoucherItem = ({ item }) => {
+    console.log(item);
+    if (
+      !item ||
+      !item.voucher ||
+      !item.voucher.store ||
+      !item.voucher.name_V ||
+      !item.voucher.discount
+    ) {
+      return null;
+    }
+
+    return (
+      <ListItem bottomDivider containerStyle={styles.voucherItem}>
+        <ListItem.Content>
+          <View style={styles.headerContainer}>
+            <Feather
+              name="shopping-bag"
               size={24}
-              color={voucherStates[item.id]?.disliked ? '#f44336' : '#9e9e9e'} // Couleur rouge pour le dislike
-              style={styles.button}
+              color="black"
+              style={styles.icon}
             />
-          </TouchableOpacity>
-        </View>
-      </ListItem.Content>
-    </ListItem>
-  );
+            <View
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+            >
+              <ListItem.Title style={styles.boutique}>
+                {item.voucher.store.store_name}
+              </ListItem.Title>
+              <View style={{ flex: 0.8 }}></View>
+
+              <ListItem.Title
+                style={{
+                  color: Colors.text,
+                  fontFamily: 'poppins',
+                  textDecorationLine: 'underline',
+                }}
+              >
+                {item.expiry.split('T')[0]}
+              </ListItem.Title>
+            </View>
+          </View>
+          <ListItem.Title style={styles.gain}>
+            <Text style={{ fontSize: 18, fontFamily: 'poppins' }}>
+              {' '}
+              Coupon :
+            </Text>
+            <Text style={{ fontSize: 18, fontFamily: 'inter' }}>
+              {' '}
+              {item.voucher.name_V}
+            </Text>
+          </ListItem.Title>
+          <ListItem.Title style={styles.gain}>
+            <Text style={{ fontSize: 16, fontFamily: 'poppins' }}>
+              {' '}
+              Réduction :{' '}
+            </Text>
+            <Text style={{ fontSize: 16, fontFamily: 'inter' }}>
+              {' '}
+              {item.voucher.discount}%
+            </Text>
+          </ListItem.Title>
+          <View style={styles.buttonsContainer}>
+            <Text style={[styles.noteText, { marginRight: 8 }]}>
+              Noter la boutique:
+            </Text>
+            <TouchableOpacity onPress={() => handleLike(item._id)}>
+              {voucherStates[item._id]?.liked ? (
+                <Ionicons
+                  name="thumbs-up"
+                  size={24}
+                  color="#4CAF50"
+                  style={styles.button}
+                />
+              ) : (
+                <Ionicons
+                  name="thumbs-up-outline"
+                  size={24}
+                  color="black"
+                  style={styles.button}
+                />
+              )}
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleDislike(item._id)}>
+              <Ionicons
+                name="thumbs-down"
+                size={24}
+                color={
+                  voucherStates[item._id]?.disliked ? '#f44336' : '#9e9e9e'
+                }
+                style={styles.button}
+              />
+            </TouchableOpacity>
+          </View>
+        </ListItem.Content>
+      </ListItem>
+    );
+  };
 
   const renderEmptyList = () => (
     <View style={styles.emptyContainer}>
-      {reservedVouchers.length === 0 ||
-      hasUsedVouchers.every((used) => used) ? (
+      {isLoading ? (
+        <Loading2 />
+      ) : (
         <View style={styles.emptyListContainer}>
           <Text style={styles.emptyListText}>
             Aucun coupon n'a été utilisé pour le moment
@@ -130,8 +243,6 @@ const HistoryScreen = () => {
             style={styles.emptyListIcon}
           />
         </View>
-      ) : (
-        <Text style={styles.emptyText}>Chargement...</Text>
       )}
     </View>
   );
@@ -141,7 +252,7 @@ const HistoryScreen = () => {
       <Text style={styles.title}>Historique des Vouchers Utilisés</Text>
       <View style={{ marginTop: 16 }}></View>
       <FlatList
-        data={reservedVouchers}
+        data={historyData}
         renderItem={renderVoucherItem}
         ListEmptyComponent={renderEmptyList}
         keyExtractor={(item) => item._id}
@@ -149,6 +260,7 @@ const HistoryScreen = () => {
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -159,6 +271,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 16,
+    textAlign: 'center',
+    marginTop: 16,
   },
   voucherItem: {
     backgroundColor: '#fff',
@@ -175,12 +289,13 @@ const styles = StyleSheet.create({
     marginRight: 8,
   },
   boutique: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
+    fontFamily: 'poppins',
+    color: Colors.background,
   },
   gain: {
-    fontSize: 14,
-    color: Colors.gray,
+    fontSize: 16,
   },
   buttonsContainer: {
     flexDirection: 'row',
@@ -189,8 +304,10 @@ const styles = StyleSheet.create({
     marginTop: 8,
   },
   noteText: {
-    fontSize: 14,
+    fontSize: 16,
     marginRight: 8,
+    fontFamily: 'poppins',
+    color: Colors.text,
   },
   button: {
     marginHorizontal: 4,
@@ -203,6 +320,7 @@ const styles = StyleSheet.create({
   emptyText: {
     fontSize: 16,
     fontWeight: 'bold',
+    color: Colors.background,
   },
   emptyListContainer: {
     justifyContent: 'center',

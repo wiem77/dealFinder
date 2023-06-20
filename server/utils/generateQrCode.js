46f1ae33124dc4de6c835419a2c199362cc93f3c
wiem77 <wiembatti77@gmail.com>
2023-06-20 13:40:44 +0100

    genarateQr code commit

--- server/utils/generateQrCode.js
+++ server/utils/generateQrCode.js
@@ -1,23 +1,20 @@
 const QRCode = require('qrcode');
 
 module.exports.generateQrCode = (stringdata, callback) => {
-  console.log('test1');
   QRCode.toString(stringdata, { type: 'terminal' }, function (err, url) {
     if (err) {
       console.log('error occurred');
       return callback(err);
     }
     console.log('QR code string:', url);
-    console.log('test2');
+
     QRCode.toDataURL(stringdata, function (err, url) {
       if (err) {
         console.log('error occurred');
         return callback(err);
       }
 
-      console.log('test3');
       callback(null, url);
     });
   });
 };
-
